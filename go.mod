3b74a5c0d8e9384aa17d3db08bb5f0dfdc8436b1
esJohan <johanchaconchacon@gmail.com>
2025-10-28 18:38:50 -0600

    reset the for the inputs

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/jjcc2000/redisPipeline
 
 go 1.21.6
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/redis/go-redis/v9 v9.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,68 @@
 package main
 
 import (
-	"log"
-	"net"
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":8080")
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+		Protocol: 2,
+	})
+
+	ctx := context.Background()
+
+	testPing(client)
+
+	SetAndGetValues(client, ctx)
+
+}
+
+func testPing(client *redis.Client) error {
+
+	ctx := context.Background()
+	fmt.Println(client.Ping(ctx))
+
+	info, err := client.ClientInfo(ctx).Result()
 	if err != nil {
-		log.Fatalf("TCP Listen Error", err)
+		return fmt.Errorf("method ClientInfo failed: %w", err)
 	}
 
-	log.Printf("Listener: %v", listener)
-	// for {
-	// 	conn, err := ln.Accept()
-	// 	if err != nil {
+	fmt.Printf("%#v\n", info)
 
-	// 	}
+	return nil
+}
 
-	// }
+func SetAndGetValues(client *redis.Client, ctx context.Context) {
+	sc := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Set the title: ")
+	sc.Scan()
+	title := sc.Text()
+
+	fmt.Println("Set the value: ")
+	sc.Scan()
+	value := sc.Text()
+
+	fmt.Println(title, value)
+	err := client.Set(ctx, title, value, 0).Err()
+
+	if err != nil {
+		fmt.Println("Error here!")
+		panic(err)
+	}
+
+	val, err := client.Get(ctx, title).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Result", val)
 }
